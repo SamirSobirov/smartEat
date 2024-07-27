753b08590f273a960c66329e949d72aa17bab3fd
Sobirov S <sobiroff23@gmail.com>
2024-07-28 02:01:16 +0500

    fixed

--- mobile.js
+++ mobile.js
@@ -1,35 +1,9 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    const content = document.getElementById('content');
-    const button = document.getElementById('toggleButton');
-    content.classList.toggle('hidden');
-    button.classList.toggle('rotate');
-});
 
 
 
-document.getElementById('toggleButtonFood').addEventListener('click', function() {
-    const content = document.getElementById('contentFood');
-    const button = document.getElementById('toggleButtonFood');
-    content.classList.toggle('hidden');
-    button.classList.toggle('rotate');
-});
-
-document.getElementById('toggleButtonCooking').addEventListener('click', function() {
-    const content = document.getElementById('contentCooking');
-    const button = document.getElementById('toggleButtonCooking');
-    content.classList.toggle('hidden');
-    button.classList.toggle('rotate');
-});
 
 
 
-document.getElementById('toggleButtonDish').addEventListener('click', function() {
-    var contentDish = document.getElementById('contentDish');
-    var toggleButton = document.getElementById('toggleButtonDish').querySelector('img');
-    
-    contentDish.classList.toggle('hidden');
-    toggleButton.classList.toggle('rotated');
-});
 
 
 
---
+++ toogles.js
@@ -0,0 +1,41 @@
+document.getElementById('toggleButtonDish').addEventListener('click', function () {
+    const contentDish = document.getElementById('contentDish');
+    const toggleButton = document.getElementById('toggleButtonDish').querySelector('img');
+
+    contentDish.classList.toggle('hidden');
+    toggleButton.classList.toggle('rotated');
+});
+
+
+document.getElementById('toggleButtonDish').addEventListener('click', function() {
+    var contentDish = document.getElementById('contentDish');
+    var toggleButton = document.getElementById('toggleButtonDish').querySelector('img');
+    
+    contentDish.classList.toggle('hidden');
+    toggleButton.classList.toggle('rotated');
+});
+
+
+document.getElementById('toggleButtonCooking').addEventListener('click', function() {
+    const content = document.getElementById('contentCooking');
+    const button = document.getElementById('toggleButtonCooking');
+    content.classList.toggle('hidden');
+    button.classList.toggle('rotate');
+});
+
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const content = document.getElementById('content');
+    const button = document.getElementById('toggleButton');
+    content.classList.toggle('hidden');
+    button.classList.toggle('rotate');
+});
+
+
+
+document.getElementById('toggleButtonFood').addEventListener('click', function() {
+    const content = document.getElementById('contentFood');
+    const button = document.getElementById('toggleButtonFood');
+    content.classList.toggle('hidden');
+    button.classList.toggle('rotate');
+});
