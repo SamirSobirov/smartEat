de0f58458f40e1aaf350490a6b91db5c1e580075
Sobirov S <sobiroff23@gmail.com>
2024-07-29 00:36:35 +0500

    fixed

--- script.js
+++ script.js
@@ -21,9 +21,8 @@ function updateMessage() {
 }
 
 
-
-//more button
-function updateMessage() {
+//kolichestvo 2 button
+function updateMessages() {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     let checkedCount = 0;
 
@@ -33,7 +32,7 @@ function updateMessage() {
         }
     });
 
-    const message = document.getElementById('message');
+    const message = document.getElementById('messages');
     const remaining = 2 - checkedCount;
 
     if (remaining > 0) {
@@ -44,10 +43,95 @@ function updateMessage() {
     }
 }
 
+
+
+
 function showMore() {
     const moreItems = document.getElementById('moreItems');
     moreItems.classList.toggle('hidden');
 }
 
+function showMore1() {
+    let moreOptions = document.getElementById('more-options');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+
+
+function showMore2() {
+    let moreOptions = document.getElementById('more-options2');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+function showMore3() {
+    let moreOptions = document.getElementById('more-options3');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+
+
+function showMore4() {
+    let moreOptions = document.getElementById('more-options4');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+function showMore5() {
+    let moreOptions = document.getElementById('more-options5');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+function showMore6() {
+    let moreOptions = document.getElementById('more-options6');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+function showMore7() {
+    let moreOptions = document.getElementById('more-options7');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
+
+
+function showMore8() {
+    let moreOptions = document.getElementById('more-options8');
+    if (moreOptions.classList.contains('hidden')) {
+        moreOptions.classList.remove('hidden');
+    } else {
+        moreOptions.classList.add('hidden');
+    }
+}
 
 
